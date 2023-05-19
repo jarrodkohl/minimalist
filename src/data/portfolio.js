8e33024bdf5c3d40799d8c646d53aeb2b4c783ea
Jarrod Kohl <jarrod.kohl@gmail.com>
2023-05-19 17:25:23 -0400

    portfolio update

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,20 +1,23 @@
 export default [
   {
-    title: "Portfolio",
-    imgUrl: "",
-    stack: ["React", "TailwindCSS"],
+    title: "PromptAi",
+    imgUrl: "assets/PromptAiScreen.png",
+    stack: ["React", "TailwindCSS", "JavaScript"],
     link: "",
+    id: 1,
   },
   {
-    title: "22Portfolio2",
-    imgUrl: "",
+    title: "Content Agent Ai",
+    imgUrl: "assets/Lemon Squeezy Web 0.png",
     stack: ["React", "TailwindCSS"],
     link: "",
+    id: 2,
   },
   {
     title: "3333Portfolio3333",
-    imgUrl: "",
+    imgUrl: "assets/Stripe Web 0.png",
     stack: ["React", "TailwindCSS"],
     link: "",
+    id: 3,
   },
 ]
---
+++ src/data/timeline.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    year: "2023",
+    title: "Full Stack Developer",
+    duration: "2021 - 2023",
+    details: "Built lots of coool stuff. This needs to be a little longer to test the layout and see how it looks",
+    id: 1, 
+  },
+  {
+    year: "2021",
+    title: "Front End Developer",
+    duration: "2019 - 2021",
+    details: "Made front ends pretty! This needs to be a little longer to test the layout and see how it looks",
+    id: 2,
+  },
+  {
+    year: "2019",
+    title: "Real Estate Developer",
+    duration: "2017 - 2019",
+    details: "The OTHER kind of developer! Built houses and stuff and sold them for money and stuff. This needs to be a little longer to test the layout and see how it looks",
+    id: 3,
+  },
+  {
+    year: "2017",
+    title: "Real Estate Agent",
+    duration: "2015 - 2017",
+    details: "Sold houses and stuff. This needs to be a little longer to test the layout and see how it looks", 
+    id: 4,
+  },
+  {
+    year: "2015",
+    title: "Real Estate Agent",
+    duration: "2013 - 2015",
+    details: "Sold houses and stuff, but not as good as the other guy. This needs to be a little longer to test the layout and see how it looks",
+    id: 5,
+  },
+
+]
