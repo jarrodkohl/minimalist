e2747a52ba4853e3d59278854b8815c4b635f52e
Jarrod Kohl <jarrod.kohl@gmail.com>
2023-07-05 18:08:46 -0400

    photo Optimize

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -106,7 +106,7 @@ const Intro = () => {
         </div>
         <div className="mt-6 md:mt-0 relative w-64 h-64 md:w-96 md:h-96">
           <img
-            src="public/assets/WRfenway0264.jpg"
+            src="public/assets/WRfenway0264-min.jpg"
             alt="picture of jarrod looking awesome"
             className="absolute inset-0 w-full h-full object-cover rounded-xl shadow-lg"
           />
